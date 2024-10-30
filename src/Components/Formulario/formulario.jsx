65dc99e87e98762c166452fe7be44b97ede13958
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-10-30 00:11:32 -0300

    criação dos inputs de formulário

--- src/Components/Formulario/formulario.jsx
+++ src/Components/Formulario/formulario.jsx
@@ -2,8 +2,15 @@ import * as S from "./formularioStyle.jsx"
 
 function Form() {
     return(
-        <>
-        </>
+            <S.Div>
+                <S.Form>
+                    <S.H1>Preencha os dados para criar o card do colaborador.</S.H1>
+                    <S.Input placeholder="Digite seu nome" />
+                    <S.Input placeholder="Digite seu cargo" />
+                    <S.Input placeholder="Informe o endereço da imagem" />
+                    <S.Input />
+                </S.Form>
+            </S.Div>
     )
 }
 
--- src/Components/Formulario/formularioStyle.jsx
+++ src/Components/Formulario/formularioStyle.jsx
@@ -1,5 +1,25 @@
 import style from "styled-components"
 
-export const P = style.p`
-color: red;
+export const Div = style.div`
+display: flex;
+justify-content: center;
+`
+
+export const Form = style.form`
+width: 70vw;
+height: 90vh;
+background-color: #80808026;
+border-radius: 1rem;
+display: flex;
+justify-content: center;
+padding: 1.5rem;
+flex-direction: column;
+`
+
+export const H1 = style.h1`
+font-size: 1.3rem;
+`
+
+export const Input = style.input`
+
 `
